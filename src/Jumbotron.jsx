6ad5ad17aa784faf3a157367638db3fb9d3cb037
The-Nick-J <151190011+The-Nick-J@users.noreply.github.com>
2024-01-16 14:54:56 +0000

    longer jumbo

--- src/Jumbotron.jsx
+++ src/Jumbotron.jsx
@@ -6,8 +6,7 @@ export const Jumbotron = () => {
       <div className="jumbotron mt-4">
         <h1 className="display-4">A Warm Welcome!</h1>
         <p className="lead">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, quod
-          laboriosam. Quasi, voluptatum. Quisquam, voluptatibus.
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio doloremque doloribus a harum neque. Quisquam ipsum veritatis magni sapiente. Dolore numquam dolorum modi tenetur fugit explicabo! Consectetur accusantium voluptatum ab? Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores ducimus itaque est vero laboriosam. Voluptatibus nam tempora tenetur quibusdam optio voluptas quos, facilis recusandae doloribus quidem adipisci. Rerum, a reprehenderit.
         </p>
         <a className="btn btn-primary btn-lg" href="#" role="button">
           Call to action!
