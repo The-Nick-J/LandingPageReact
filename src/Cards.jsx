8f1a6a6249881afb22ef3770370c3e73dd142b34
damianconpan <146871059+damianconpan@users.noreply.github.com>
2024-01-16 12:18:44 -0600

    Update Cards.jsx

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Cards = () => {
     return (
-        <div>
+        <div className="d-flex" >
             <div className="card" style={{width: '18rem'}}>
                 <img src="..." className="card-img-top" alt="..." />
                 <div className="card-body">
